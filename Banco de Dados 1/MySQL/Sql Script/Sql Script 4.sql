e10315d06d38b58f25a10195cb101dc1265d0931
Leonardo Barreto <118021374+lbrrt11@users.noreply.github.com>
2023-10-27 10:06:31 -0300

    Update Sql Script 4.sql

--- Banco de Dados 1/MySQL/Sql Script/Sql Script 4.sql
+++ Banco de Dados 1/MySQL/Sql Script/Sql Script 4.sql
@@ -1,14 +1,14 @@
 create database Teste;
 use Teste;
 
-create table Aluno13(
+create table Aluno(
 no_cartao int,
 nome VarChar(45),
 
 primary key (no_cartao)
 );
 
-create table Sala13(
+create table Sala(
 num_predio int auto_increment,
 num_sala int,
 capacidade int,
@@ -16,24 +16,22 @@ capacidade int,
 primary key (num_predio, num_sala)
 );
 
-create table Aula13(
+create table Aula(
 dia date,
 hora time,
 sala_num_predio int,
 sala_num_sala int,
 
 primary key (dia, hora, sala_num_predio, sala_num_sala),
-foreign key (sala_num_predio) references Sala13 (num_predio),
-foreign key (sala_num_sala) references Sala13 (num_sala)
+foreign key (sala_num_predio, sala_num_sala) references Sala (num_predio, num_sala)
 );
 
-create table Presença13(
+create table Presença(
 aluno_no_cartao int,
 aula_dia date,
 aula_hora time,
 
 primary key (aluno_no_cartao, aula_dia, aula_hora),
-foreign key (aluno_no_cartao) references Aluno13 (no_cartao),
-foreign key (aula_dia) references Aula13 (dia),
-foreign key (aula_hora) references Aula13 (hora)
+foreign key (aluno_no_cartao) references Aluno (no_cartao),
+foreign key (aula_dia, aula_hora) references Aula (dia, hora)
 );

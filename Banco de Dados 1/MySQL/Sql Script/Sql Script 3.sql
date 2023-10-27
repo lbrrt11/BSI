b6497ebc11eb93f7518d9b9a4cd7550af6ef3e62
Leonardo Barreto <118021374+lbrrt11@users.noreply.github.com>
2023-10-27 10:19:41 -0300

    Update Sql Script 3.sql

--- Banco de Dados 1/MySQL/Sql Script/Sql Script 3.sql
+++ Banco de Dados 1/MySQL/Sql Script/Sql Script 3.sql
@@ -1,7 +1,7 @@
--- create database Empresa;
+create database Empresa;
 use Empresa;
 
-create table Empregado7(
+create table Empregado(
 pnome VarChar(45),
 minicial VarChar(45),
 unome Varchar(45),
@@ -14,48 +14,48 @@ dnumero int,
 superssn int,
 
 primary key (ssn),
-foreign key (superssn) references Empregado7 (ssn)
+foreign key (superssn) references Empregado (ssn)
 );
 
-create table Departamento7(
+create table Departamento(
 dnome VarChar(45),
 dnumero int auto_increment,
 gerssn int,
 gerdatainicio date,
 
 primary key (dnumero),
-foreign key (gerssn) references Empregado7 (ssn)
+foreign key (gerssn) references Empregado (ssn)
 );
 
-create table Depto_localizacoes7(
+create table Depto_localizacoes(
 dnumro int,
 dlocalizacao VarChar(45),
 
 primary key (dnumro, dlocalizacao),
-foreign key (dnumro) references Departamento7 (dnumero)
+foreign key (dnumro) references Departamento (dnumero)
 );
 
-create table Projeto7(
+create table Projeto(
 pjnome VarChar(45),
 pnumero int auto_increment,
 plocalizacao varchar(45),
 dnum int,
 
 primary key (pnumero),
-foreign key (dnum) references Departamento7 (dnumero)
+foreign key (dnum) references Departamento (dnumero)
 );
 
-create table Trabalha_em7(
+create table Trabalha_em(
 essn int auto_increment,
 pn int,
 horas int,
 
 primary key (essn, pn),
-foreign key (essn) references Empregado7 (ssn),
-foreign key (pn) references Projeto7 (pnumero)
+foreign key (essn) references Empregado (ssn),
+foreign key (pn) references Projeto (pnumero)
 );
 
-create table Dependente7(
+create table Dependente(
 essn int,
 nome_dependente VarChar(45),
 sexo varchar(45),
@@ -63,5 +63,5 @@ datanasc date,
 parentesco varchar(45),
 
 primary key (essn, nome_dependente),
-foreign key (essn) references Empregado7 (ssn)
+foreign key (essn) references Empregado (ssn)
 );

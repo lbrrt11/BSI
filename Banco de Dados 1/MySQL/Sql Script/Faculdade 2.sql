d49660ea36f77341a23c5929eec9b9ba3cf4e30d
Leonardo Barreto <118021374+lbrrt11@users.noreply.github.com>
2023-11-17 11:36:25 -0300

    Update Faculdade 2.sql

--- Banco de Dados 1/MySQL/Sql Script/Faculdade 2.sql
+++ Banco de Dados 1/MySQL/Sql Script/Faculdade 2.sql
@@ -1,5 +1,5 @@
 create database Faculdade2;
-use Faculdade;
+use Faculdade2;
 
 create table Aluno(
 matricula int unique,
@@ -64,3 +64,31 @@ primary key (aluno_matricula, turma_tnumero),
 foreign key (aluno_matricula) references Aluno (matricula),
 foreign key (turma_tnumero) references Turma (tnumero)
 );
+
+insert into Aluno(nome, matricula, tipo, curso) values ('Carlos', '001', 'Graduação', 'Sistemas de Informação');
+insert into Aluno(nome, matricula, tipo, curso) values ('Rafael', '101', 'Pós-Graduação', 'Mestrado em Filosofia');
+insert into Aluno(nome, matricula, tipo, curso) values ('Laura', '002', 'Graduação', 'Sistemas de Informação');
+
+insert into Disciplina(dnumero, nome, carga_horaria, depnumero) values ('03', 'Banco de Dados', '64', '02');
+insert into Disciplina(dnumero, nome, carga_horaria, depnumero) values ('02', 'Filosofia, Ética e Desenvolvimento', '16', '03');
+insert into Disciplina(dnumero, nome, carga_horaria, depnumero) values ('01', 'Inglês Instrumental', '32', '01');
+
+insert into Departamento(nome, depnumero) values ('Departamento A', '01');
+insert into Departamento(nome, depnumero) values ('Departamento B', '02');
+insert into Departamento(nome, depnumero) values ('Departamento C', '03');
+
+insert into Pre_requisito(disciplina_dnumero, dnumero_pre) values ('03', '02');
+insert into Pre_requisito(disciplina_dnumero, dnumero_pre) values ('02', '01');
+insert into Pre_requisito(disciplina_dnumero, dnumero_pre) values ('01', '03');
+
+insert into Professor(pnumero, nome, departamento_depnumero) values ('04', 'Alvaro', '01');
+insert into Professor(pnumero, nome, departamento_depnumero) values ('09', 'Gisele', '02');
+insert into Professor(pnumero, nome, departamento_depnumero) values ('03', 'Paulo', '03');
+
+insert into Turma(tnumero, disciplina_dnumero, ano, semestre, professor_pnumero) values ('01', '03', '2020', '6', '04');
+insert into Turma(tnumero, disciplina_dnumero, ano, semestre, professor_pnumero) values ('02', '02', '2021', '4', '09');
+insert into Turma(tnumero, disciplina_dnumero, ano, semestre, professor_pnumero) values ('03', '01', '2022', '2', '03');
+
+insert into Historico(aluno_matricula, turma_tnumero, nota) values ('001', '01', '8.5');
+insert into Historico(aluno_matricula, turma_tnumero, nota) values ('101', '02', '6.8');
+insert into Historico(aluno_matricula, turma_tnumero, nota) values ('002', '03', '9.4');

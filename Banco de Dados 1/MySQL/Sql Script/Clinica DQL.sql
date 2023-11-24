cac054a9d55e8f897ceb81fcd0a99996ab82e812
Leonardo Barreto <118021374+lbrrt11@users.noreply.github.com>
2023-11-24 10:30:06 -0300

    Update Clinica DQL.sql

--- Banco de Dados 1/MySQL/Sql Script/Clinica DQL.sql
+++ Banco de Dados 1/MySQL/Sql Script/Clinica DQL.sql
@@ -24,42 +24,58 @@ where especialidade = 'ortopedia';
 
 -- QUESTÃO 1 F
 select count(cdAmb) -- count(cdAmb) conta as ocorrências
-from ambulatorio;
+from medico
+where cdAmb is not null;
 
 -- QUESTÃO 1 G
-select count(capacidade)
+select sum(capacidade)
 from ambulatorio
 where andar = '1';
 
 -- QUESTÃO 1 H
-select avg(idade) -- avg() calcula média
+select round(avg(idade),1) -- avg() calcula média
 from paciente
 where cidade = 'Itapetinga';
 
 -- QUESTÃO 1 I
-select min(salario), max(salario)
+select min(salario) as mínimo, max(salario) as máximo
 from funcionario
 where idade > '35';
 
 -- QUESTÃO 1 J
-select count(especialidade)
+select count(distinct (especialidade)) as especialidades
 from medico;
 
 -- QUESTÃO 1 K
-select distinct avg(salario)
+select avg(distinct(salario))
 from funcionario;
 
 -- QUESTÃO 1 L
 select nome, CPF
 from medico
 where idade < '40'
-and especialidade != 'ortopedia';
+or especialidade <> 'ortopedia';
 
 -- QUESTÃO 1 M
 select cdCons, cdMed, cdPac, cdFunc_Marcacao, data, hora, Obs
 from consulta
 where hora between '13:00' and '19:00';
 --- and data after '01/01/2023';
+
+-- QUESTÃO 1 N
+select nome, idade, cidade
+from paciente
+where cidade <> 'Itapetinga';
+
+-- QUESTÃO 1 O
+select hora
+from consulta
+where data < '2023-04-30' or data > '2023-05-10';
+
+-- QUESTÃO 1 P
+select nome, especialidade
+from medico
+where nome like '_o%o';
 
 -- QUESTÃO 1 Q
 select count(cdPac), cidade

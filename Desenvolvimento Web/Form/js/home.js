4c2ca7e6a5e7dd4da87134a705ee39b5a8305e46
Leonardo Barreto <118021374+lbrrt11@users.noreply.github.com>
2024-11-23 13:15:34 -0300

    Update home.js

--- Desenvolvimento Web/Form/js/home.js
+++ Desenvolvimento Web/Form/js/home.js
@@ -4,7 +4,9 @@ const usernameDisplay = document.getElementById('username-display');
 const currentUser = users.find(user => user.username === localStorage.getItem('currentUser'));
 
 if (currentUser) {
-    usernameDisplay.textContent = currentUser.username; // Exibe o nome do usuário logado
-} else {
-    usernameDisplay.textContent = "Usuário não encontrado"; // Caso não encontre o usuário
+    usernameDisplay.textContent = currentUser.username;
+}
+
+else {
+    usernameDisplay.textContent = "Usuário não encontrado";
 }
